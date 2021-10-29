8e8a6571818d4d2dc0d79e692057b02a0b9444aa
srisaikumar-sys <srisaikumar.sk@outlook.com>
2021-10-29 11:27:42 +0530

    Master

---
+++ src/test/java/org/test/Facebook_Login.java
@@ -0,0 +1,22 @@
+package org.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Facebook_Login {
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sri Sai kumar\\eclipse-workspace\\Selinium_First_Project\\Drivers\\chromedriver.exe");
+
+		WebDriver driver= new ChromeDriver();
+
+		driver.get("https://phptravels.org/index.php?rp=/login");
+		
+		WebElement txtUserName = driver.findElement(By.id("inputEmail"));
+		txtUserName.sendKeys("srisai.veerakumar@gmail.com");
+		
+	}
+
+}
