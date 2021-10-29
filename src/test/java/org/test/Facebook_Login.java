e6e2fd39ca1e45f15aae091ecdf79da1df8d72f9
srisaikumar-sys <srisaikumar.sk@outlook.com>
2021-10-29 12:18:01 +0530

    Sai

--- src/test/java/org/test/Facebook_Login.java
+++ src/test/java/org/test/Facebook_Login.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Facebook_Login {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Sri Sai kumar\\eclipse-workspace\\Selinium_First_Project\\Drivers\\chromedriver.exe");
 
@@ -22,6 +22,14 @@ public class Facebook_Login {
 		
 		WebElement btnClick = driver.findElement(By.name("Button"));
 		btnClick.click();
+		
+		Thread.sleep(300);
+		System.out.println("Facebook");
+		System.out.println("Insta");
+
+		System.out.println("Whatsapp");
+
+	
 
 		
 	}
